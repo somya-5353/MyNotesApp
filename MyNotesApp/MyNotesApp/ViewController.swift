4c269a768ea94e406ea322029b66b9de5416b6ce
Somya-5353 <somyacarmel5353@gmail.com>
2020-06-26 18:58:39 +0530

    initial project setup commit

---
+++ MyNotesApp/MyNotesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyNotesApp
+//
+//  Created by Somya on 26/06/20.
+//  Copyright © 2020 Somya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
