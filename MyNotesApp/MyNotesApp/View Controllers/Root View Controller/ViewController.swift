2370c0a0c1ac11cbf99d881e966121b08fc29f4e
Somya-5353 <somyacarmel5353@gmail.com>
2020-07-02 17:59:56 +0530

    adding entities and relationships and floating buttton

--- MyNotesApp/MyNotesApp/View Controllers/Root View Controller/ViewController.swift
+++ MyNotesApp/MyNotesApp/View Controllers/Root View Controller/ViewController.swift
@@ -5,7 +5,7 @@
 //  Created by Somya on 26/06/20.
 //  Copyright © 2020 Somya. All rights reserved.
 //
-
+import CoreData
 import UIKit
 
 class ViewController: UIViewController {
@@ -15,9 +15,33 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.setCoreData()
     }
 
+   //function to setUp CoreData
+    func setCoreData() {
+        
+        //getting entity description
+        if let entityDesc = NSEntityDescription.entity(forEntityName: "Note", in: coreDataManager.managedObjectContext) {
+       //initializing managed object
+            let note = NSManagedObject(entity: entityDesc, insertInto: coreDataManager.managedObjectContext)
+        
+      //configuring managed object
+            note.setValue("My First Note", forKey: "title")
+            note.setValue(NSDate(), forKey: "createdAt")
+            note.setValue(NSDate(), forKey: "updatedAt")
+            
+            if let title = note.value(forKey: "title") as? String {
+                print(title)
+            }
+      //saving managedObjectContext to the persistence Store
+            do {
+                try coreDataManager.managedObjectContext.save()
+            } catch {
+                print("Unable to save managed object context!")
+            }
+     }
+    
+  }
 
 }
-
