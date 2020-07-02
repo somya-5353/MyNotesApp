14de6eefcbd823eb4e14adb1be26bbd3164f5a68
Somya-5353 <somyacarmel5353@gmail.com>
2020-07-02 21:38:56 +0530

    buttons and animations added to menu view

--- MyNotesApp/MyNotesApp/View Controllers/Root View Controller/ViewController.swift
+++ MyNotesApp/MyNotesApp/View Controllers/Root View Controller/ViewController.swift
@@ -9,30 +9,40 @@ import CoreData
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     
     @IBOutlet weak var buttonView: UIView!
-    
+    @IBOutlet weak var addNoteButton: UIButton!
     @IBOutlet weak var floatingButton: UIButton!
+    @IBOutlet weak var notificationButton: UIButton!
     
     private var coreDataManager = CoreDataManager(modelName: "Notes")
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        self.buttonView.transform = CGAffineTransform(scaleX: 0.001, y: 0.001)
-        self.buttonView.layer.cornerRadius = 170.0
+        self.setUpButtonMenu()
         self.setCoreData()
     }
-
-   //function to setUp CoreData
+    
+    //function for initial settings of the button menu
+    func setUpButtonMenu() {
+        
+        self.buttonView.transform = CGAffineTransform(scaleX: 0.001, y: 0.001)
+        self.buttonView.layer.cornerRadius = 170.0
+        self.addNoteButton.transform = CGAffineTransform(translationX: 15, y: 15)
+        self.notificationButton.transform = CGAffineTransform(translationX: 15, y: 15)
+    }
+    
+    //function to setUp CoreData
     func setCoreData() {
         
         //getting entity description
         if let entityDesc = NSEntityDescription.entity(forEntityName: "Note", in: coreDataManager.managedObjectContext) {
-       //initializing managed object
+            //initializing managed object
             let note = NSManagedObject(entity: entityDesc, insertInto: coreDataManager.managedObjectContext)
-        
-      //configuring managed object
+            
+            //configuring managed object
             note.setValue("My First Note", forKey: "title")
             note.setValue(NSDate(), forKey: "createdAt")
             note.setValue(NSDate(), forKey: "updatedAt")
@@ -40,48 +50,50 @@ class ViewController: UIViewController {
             if let title = note.value(forKey: "title") as? String {
                 print(title)
             }
-      //saving managedObjectContext to the persistence Store
+            //saving managedObjectContext to the persistence Store
             do {
                 try coreDataManager.managedObjectContext.save()
             } catch {
                 print("Unable to save managed object context!")
             }
-       }
-   }
-   
-    func animateView(isHidden: Bool) {
-        
-        UIView.animate(withDuration: 0.3, delay: 0.05, options: .curveEaseOut, animations: {
-            self.buttonView.isHidden = isHidden
-        }, completion: nil)
+        }
     }
     
     func beginTracking() {
         
         UIView.animate(withDuration: 0.3, animations: {
             if(self.floatingButton.alpha == 1) {
-            self.floatingButton.alpha = 0.7
+                self.floatingButton.alpha = 0.7
             } else {
                 self.floatingButton.alpha = 1
             }
             if(self.floatingButton.transform == .identity) {
-            self.floatingButton.transform = CGAffineTransform(rotationAngle: 45 * (.pi/180))
+                self.floatingButton.transform = CGAffineTransform(rotationAngle: 45 * (.pi/180))
                 self.buttonView.transform = .identity
             } else {
                 self.floatingButton.transform = .identity
                 self.buttonView.transform = CGAffineTransform(scaleX: 0.001, y: 0.001)
             }
+            self.animateMenuButtons()
         })
         
     }
     
-    func endTracking() {
+    func animateMenuButtons() {
         
-        UIView.animate(withDuration: 0.3, animations: {
-            self.floatingButton.alpha = 1
-        })
-        print(self.floatingButton.state)
-        self.floatingButton.setImage(UIImage(named: "cross"), for: .normal)
+        UIView.animate(withDuration: 0.5, delay: 0.25, usingSpringWithDamping: 0.3, initialSpringVelocity: 0, options: [], animations: {
+            
+            if(self.addNoteButton?.transform == .identity) {
+                self.addNoteButton.transform = CGAffineTransform(translationX: 15, y: 15)
+            } else {
+                self.addNoteButton.transform = .identity
+            }
+            if(self.notificationButton?.transform == .identity) {
+                self.notificationButton.transform = CGAffineTransform(translationX: 15, y: 15)
+            } else {
+                self.notificationButton.transform = .identity
+            }
+        }, completion: nil)
     }
     
     @IBAction func onTapFloatingButton(_ sender: Any) {
